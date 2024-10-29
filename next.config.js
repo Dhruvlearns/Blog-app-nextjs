b5d80439920227fb51cf030a3b07f4322b5abf6a
Dhruv Agrawal <dhruv.alearner@gmail.com>
2024-10-29 22:59:24 +0530

    updated

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images:{
-    domains:["lh3.googleusercontent.com","firebasestorage.googleapis.com"]
-  }
-}
-
-module.exports = nextConfig
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        encoding: false,
+      };
+    }
+    return config;
+  },
+};
